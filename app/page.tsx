5b5ddebb258a38a6454f25bea6da4abde5e353e2
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-02-19 09:44:46 -0300

    Feat: basic styles for main page
    add simple dak style for my personal portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 export default function Home() {
   return (
-    <main>
-      <div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <h1>Hello Guys</h1>
       </div>
     </main>
